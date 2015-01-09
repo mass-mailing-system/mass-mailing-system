a98895f76bca8f8636b0de25e373f62837f46604
Kozak Oleg <mckey1992@gmail.com>
2015-01-09 22:21:32 +0200

    Created and refactoring base entity for Messege && Contact

--- entity/src/main/java/com/mass/mailing/system/contact/Contact.java
+++ entity/src/main/java/com/mass/mailing/system/contact/Contact.java
@@ -11,4 +11,51 @@ package com.mass.mailing.system.contact;
  * @author Oleg Kozak
  * @author Illia Rudenko
  */
-public interface Contact {}
+public class Contact {
+    protected Long id;
+    
+    private String email;
+    private String phoneNumber;
+    
+    /**
+     * @return the resourseTo
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the resourseTo to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the phoneNumber
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * @param phoneNumber the phoneNumber to set
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
