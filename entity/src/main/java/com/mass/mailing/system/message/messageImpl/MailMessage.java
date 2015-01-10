6e4d23e66f7179941dca9a0ccfab400f85964b46
Illia <illiarudenko52@gmail.com>
2015-01-10 21:16:04 +0200

    Initial message implementation structure (have no idea how to deal with messengers)

---
+++ entity/src/main/java/com/mass/mailing/system/message/messageImpl/MailMessage.java
@@ -0,0 +1,16 @@
+package com.mass.mailing.system.message.messageImpl;
+
+
+import com.mass.mailing.system.message.AbstractMessage;
+
+/**
+ * @author Illia Rudenko
+ */
+public class MailMessage extends AbstractMessage {
+
+
+    @Override
+    public void send() {
+        //TODO: consider how to implement
+    }
+}
